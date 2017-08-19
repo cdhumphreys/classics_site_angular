c3ee409a626b54a57daf2b9b32aa6f206eae3d84
cdhumphreys <humphreys.carl.d@gmail.com>
2017-08-19 16:52:39 +0100

    add question html and form setup, need to connect to db

--- classics-site/src/app/app.module.ts
+++ classics-site/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -58,6 +58,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
--- classics-site/src/app/components/admin/admin.component.ts
+++ classics-site/src/app/components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
   selectedOption: string;
+
+  // populate this from db
+  books = [1,2,3,4,5];
+
+  questionForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
   }
 
   onSelectOption(selectedOption: string, event) {
@@ -31,6 +38,40 @@ export class AdminComponent implements OnInit {
     }
 
     this.selectedOption = selectedOption;
+  }
+
+  private initForm() {
+    let book = '';
+    let question = '';
+    let answers = new FormArray([
+      new FormGroup({
+        'answer': new FormControl('')
+      })
+    ]);
+    let correctAnswer = '';
+    let explanation = '';
+
+    if (this.selectedOption == 'edit') {
+      // populate with selected question data
+    }
+
+    this.questionForm = new FormGroup({
+      'book': new FormControl(book, Validators.required),
+      'question': new FormControl(question, Validators.required),
+      'answers': answers,
+      'correctAnswer': new FormControl(correctAnswer, Validators.required),
+      'explanation': new FormControl(explanation),
+    });
+  }
+
+  onAddAnswer() {
+    (<FormArray>this.questionForm.get('answers')).push(new FormGroup({
+      'answer': new FormControl(null, Validators.required)
+    }));
+  }
+
+  onRemoveAnswer() {
+    (<FormArray>this.questionForm.get('answers')).removeAt((<FormArray>this.questionForm.get('answers')).length-1);
   }
 
 }
